369b011533e0867458fd33e6454cf69497e0b293
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-18 15:21:42 +0700

    [update] another change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 const siteMetadata = {
-  title: 'Fiqry choerudin 👋',
+  title: 'Fiqry choerudin',
   author: 'Fiqry choerudin',
-  headerTitle: '',
+  headerTitle: 'Fiqrychoerudin.dev',
   description:
     'I was born in 1999. Being programmer since college in University. working as Frontend Developer. love making open source projects on Github.',
   language: 'en-us',
   siteUrl: 'https://fiqrychoerudin.dev',
   siteRepo: 'https://github.com/fiqryq/Fiqrychoerudin.dev',
-  siteLogo: '👋',
+  siteLogo: '/public/static/images/avatar.png',
   image: '/public/static/images/avatar.png',
   socialBanner: '/public/static/images/banner.png',
   email: 'halo@fiqrychoerudin.dev',
@@ -18,55 +18,35 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/fiqrychoerudin',
   locale: 'en-US',
   analytics: {
-    // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: true, // true or false
-    googleAnalyticsId: 'G-8LX651HGWT', // e.g. UA-000000-2 or G-XXXXXXX
+    plausibleDataDomain: '',
+    simpleAnalytics: true,
+    googleAnalyticsId: 'G-8LX651HGWT',
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit
-    // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
   comment: {
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: 'giscus',
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      mapping: 'pathname',
+      reactions: '1',
       metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
       theme: 'dark',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
     utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+      issueTerm: '',
+      label: '',
       theme: '',
-      // theme when dark mode
       darkTheme: '',
     },
     disqusConfig: {
-      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
       shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
     },
   },
