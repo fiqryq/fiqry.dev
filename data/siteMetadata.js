483a178cd305e6c2c5d8b43a670ccec7abae3c1b
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-18 15:00:06 +0700

    [update] meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,8 +20,8 @@ const siteMetadata = {
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    simpleAnalytics: true, // true or false
+    googleAnalyticsId: 'G-8LX651HGWT', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit
