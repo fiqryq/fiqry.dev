be6187883c41f24af298e60a4791910d913fcef7
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-20 16:28:22 +0700

    [update] projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import projectsData from '@/data/projectsData'
+import githubData from '@/data/githubData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 
@@ -10,6 +11,9 @@ export default function Projects() {
       <div className="">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <span role="img" className="mr-4" aria-label="wave">
+              🚧
+            </span>
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Showcase my project.</p>
@@ -25,6 +29,32 @@ export default function Projects() {
                 href={d.href}
               />
             ))}
+          </div>
+        </div>
+        <div className="container py-5">
+          <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              <span role="img" className="mr-4" aria-label="wave">
+                📦️
+              </span>
+              Github Project
+            </h1>
+            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+              Showcase my github project.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            {githubData.map((d) => {
+              return (
+                <Card
+                  key={d.title}
+                  title={d.title}
+                  description={d.description}
+                  imgSrc={d.imgSrc}
+                  href={d.href}
+                />
+              )
+            })}
           </div>
         </div>
       </div>
