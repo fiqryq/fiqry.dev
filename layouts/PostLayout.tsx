0d9c88c68e339a4222ecfa90fc1e0b9a06207dbe
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-23 13:21:35 +0700

    [update] learn docker

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -61,7 +61,7 @@ export default function PostLayout({ frontMatter, authorDetails, children }: Pro
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-4 xl:row-span-2">
-              <div className="pt-3 pb-8 prose dark:prose-dark max-w-none">{children}</div>
+              <div className="pt-2 pb-8 prose dark:prose-dark max-w-none">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
               </div>
