d9f2650bf891689dd78ea6bd7b8069ad1d5a8715
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2022-01-17 20:57:27 +0700

    [fix] fix footer flex direction

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col space-y-6 justify-start py-10 mb-0 space-x-0 text-md text-gray-500 dark:text-gray-400">
         <NowPlaying />
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0">
           <ul className="flex space-x-2">
             <li>{`© ${new Date().getFullYear()}`}</li>
             <li>{` • `}</li>
