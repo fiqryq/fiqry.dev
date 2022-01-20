377be6f905110b472f386603a9c89b88c0a69aae
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2022-01-20 23:31:30 +0700

    update site

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,59 +21,50 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
     <>
       <PageSEO title={siteMetadata.author} description={siteMetadata.description} />
       <div className="divide-gray-200 dark:divide-gray-700">
-        <div className="mb-5 py-12 space-y-2 rounded-lg md:space-y-5 bg-gradient-to-r from-cyan-500 to-blue-500 p-10">
+        <div className="py-10 space-y-2 rounded-lg md:space-y-5">
           <div className="flex flex-col space-y-3">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-white dark:text-white sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-800 dark:text-white sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
               {`Hi 👋 i'm ${siteMetadata.author}`}
             </h1>
-            <p className="text-lg leading-7 text-white dark:text-white">
+            <p className="text-lg leading-7 text-gray-800 dark:text-white">
               {siteMetadata.description}
+              <span className="font-bold text-sky-600 ml-3 hover:underline hover:underline-offset-3">
+                <Link href="/about">Read About Me &rarr;</Link>
+              </span>
             </p>
-            <div className="flex space-x-2 pt-3">
+            {/* <div className="flex space-x-2 pt-3">
               <Link href="/blog">
                 <div className="w-28 h-12 rounded-md bg-sky-700 hover:bg-sky-800 shadow-lg shadow-sky-600/50 flex items-center justify-center">
                   <p className="text-white">Read Blog</p>
                 </div>
               </Link>
               <Link href="/about">
-                <div className="w-28 h-12 rounded-md bg-sky-700 hover:bg-sky-800 shadow-lg shadow-sky-700/50 flex items-center justify-center">
+                <div className="w-28 h-12 rounded-md bg-emerald-500 hover:bg-emerald-800 shadow-lg shadow-sky-700/50 flex items-center justify-center">
                   <p className="text-white">About Me</p>
                 </div>
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
         <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-          <span className="pr-2" role="img" aria-label="wave">
-            📒
-          </span>
           Recent Post
         </h1>
-        <ul className="sdivide-gray-200 dark:divide-gray-700">
+        <ul className="sdivide-gray-200">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
             const { slug, title, summary, tags } = frontMatter
             return (
-              <li key={slug} className="py-3">
+              <li key={slug} className="py-2">
                 <Link href={`/blog/${slug}`} aria-label={`Read "${title}"`}>
-                  <article className="bg-gray-200 bg-opacity-20 hover:bg-gray-300 hover:bg-opacity-30 dark:bg-gray-900 dark:hover:bg-black p-5 rounded-lg">
-                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                      <div className="space-y-5 xl:col-span-4">
-                        <div className="space-y-3">
-                          <h2 className="text-3xl font-bold leading-8 tracking-tight">
-                            <Link href={`/blog/${slug}`} className="text-cyan-600">
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                          <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                            {summary}
-                          </div>
-                        </div>
+                  <article className="space-y-2 hover:bg-gray-200 hover:bg-opacity-30 gap-3 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline bg-gray-200 bg-opacity-20 dark:bg-gray-900 dark:hover:bg-black p-5">
+                    <div className="space-y-1 xl:col-span-4">
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link href={`/blog/${slug}`} className="text-sky-600 ">
+                          {title}
+                        </Link>
+                      </h3>
+                      <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+                        {summary}
                       </div>
                     </div>
                   </article>
@@ -87,10 +78,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-cyan-500 hover:text-cyan-600 dark:hover:text-cyan-400"
+            className="text-sky-600 font-bold hover:text-sky-700 dark:hover:text-sky-700"
             aria-label="all posts"
           >
-            All Posts &rarr;
+            Read All Post &rarr;
           </Link>
         </div>
       )}
@@ -99,9 +90,6 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       </div>
       <div className="py-7">
         <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-          <span className="pr-2" role="img" aria-label="wave">
-            🎧
-          </span>
           My <span className="text-green-500">Spotify</span> Top Song
         </h1>
         <TopTracks />
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,9 +11,6 @@ export default function Projects() {
       <div className="">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5 ">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            <span role="img" className="mr-4" aria-label="wave">
-              ⚙
-            </span>
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
