cebc4fb5c6af42e84fd69d3e88ee42892ba6a174
fiqryq <fiqrychoerudin48@gmail.com>
2022-11-16 09:31:43 +0700

    refactor: revamp

--- components/SectionContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-interface Props {
-  children: ReactNode
-}
-
-export default function SectionContainer({ children }: Props) {
-  return <div className="max-w-2xl px-4 mx-auto sm:px-6 xl:max-w-2xl xl:px-0">{children}</div>
-}
--- pages/snippets.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function snippets() {
-  return (
-    <div>
-      <p>halo</p>
-    </div>
-  )
-}
