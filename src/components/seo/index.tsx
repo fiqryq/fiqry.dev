e34828b7ac0ba6bb5e8431db7b147b2716f0b664
fiqryq <fiqrychoerudin48@gmail.com>
2022-12-25 22:41:58 +0700

    feat: dynamic og image , fix seo #42

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -1,4 +1,5 @@
 import { siteMetadata } from '@/constant/meta-data';
+import { SITE_URL as URL } from '@/constant/global';
 import Head from 'next/head';
 interface Props {
   title?: string;
@@ -6,6 +7,7 @@ interface Props {
   canonicalUrl?: string;
   ogTwitterImage?: any;
   ogType?: any;
+  ogTitle?: string;
 }
 
 const HeadSeo: React.FC<Props> = ({
@@ -13,7 +15,8 @@ const HeadSeo: React.FC<Props> = ({
   description,
   canonicalUrl,
   ogTwitterImage,
-  ogType
+  ogType,
+  ogTitle
 }) => {
   return (
     <Head>
@@ -25,14 +28,14 @@ const HeadSeo: React.FC<Props> = ({
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogTwitterImage} />
-      <link rel="canonical" href={canonicalUrl} />
       <meta property="og:locale" content="en_US" />
       <meta property="og:site_name" content={siteMetadata.siteName} />
       <meta property="og:type" content={ogType} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/public/favicon.ico" />
+      <meta property="og:image" content={`${URL}/api/og?title=${ogTitle}`} />
       <meta property="og:url" content={canonicalUrl} />
+      <link rel="canonical" href={canonicalUrl} />
     </Head>
   );
 };
---
+++ src/constant/global.ts
@@ -0,0 +1,1 @@
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
