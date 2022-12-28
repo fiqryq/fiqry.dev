c101978cc66741a4bc7cb9e216405bc15b351d0a
fiqryq <fiqrychoerudin48@gmail.com>
2022-12-28 22:24:35 +0700

    feat: add constant

---
+++ src/constant/404-constant.ts
@@ -0,0 +1,17 @@
+export const forurofour = [
+  {
+    title: 'Blog',
+    description: 'A blog about my thoughts.',
+    slug: '/blog'
+  },
+  {
+    title: 'Project',
+    description: 'A page about my project list.',
+    slug: '/project'
+  },
+  {
+    title: 'About',
+    description: 'A page about me.',
+    slug: '/about'
+  }
+];
